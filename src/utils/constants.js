93b260d7aed8636ffbbfc9a3a31e81859f2be08d
Everson Eduardo <137198071+Evers0nn@users.noreply.github.com>
2026-07-16 11:35:49 -0300

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const COLORS = { 
+  dark: '#033859', 
+  deep: '#025E73', 
+  teal: '#038C8C', 
+  aqua: '#84BFB9', 
+  bg: '#F2F2EB' 
+};
+
+export const CHART_COLORS = [COLORS.dark, COLORS.deep, COLORS.teal, COLORS.aqua];
+
+export const API_URL = 'https://financeiro-backend-7pzo.onrender.com/api';
