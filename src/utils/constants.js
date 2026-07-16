ac6049a5a9b10b9e1ba61a27b8782c40ee03474a
Everson Eduardo <137198071+Evers0nn@users.noreply.github.com>
2026-07-16 14:00:37 -0300

    Fix API_URL export formatting in constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,5 +5,7 @@ export const COLORS = {
   aqua: '#84BFB9', 
   bg: '#F2F2EB' 
 };
+
 export const CHART_COLORS = [COLORS.dark, COLORS.deep, COLORS.teal, COLORS.aqua];
+
 export const API_URL = 'https://financeiro-backend-7pzo.onrender.com/api';
